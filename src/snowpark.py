b6481d9c15d6e73a23f9ac631a25f14fff0c04fd
Anandhan, kathiresan <kathiresan.anandhan@astrazeneca.com>
2022-11-16 17:31:10 +0530

    Adding webapp

--- src/snowpark.py
+++ src/snowpark.py
@@ -14,9 +14,10 @@ def create_session():
     return new_session
 
 #testing snowpark conntectivity
-#session = create_session()
+session = create_session()
 #CommonConditonsinPatients = session.sql("SELECT CONDITION, count(*) FROM drugsComTrain_raw GROUP BY CONDITION").to_df("Conditions","Count")
 
+#CommonConditonsinPatients = CommonConditonsinPatients.na.drop()
 #print(CommonConditonsinPatients.show())
 
 #query = session.sql("SELECT RATING, count(*) FROM drugsComTrain_raw GROUP BY RATING ").to_df("Ratings","Count")
@@ -28,5 +29,31 @@ def create_session():
 #drugUsage = session.sql("SELECT COUNT(DISTINCT CONDITION) as drugusagecount, DRUGNAME FROM drugsComTrain_raw GROUP BY DRUGNAME HAVING COUNT(DISTINCT CONDITION) >1")
 #print(drugUsage.show())
 
-#query = session.sql("SELECT COUNT(*) FROM drugsComTrain_raw where RATING > 1 and RATING <=3")
-#print(query.show())
+#prepared_train_table = session.sql("SELECT * FROM drugsComTrain_raw where CONDITION NOT LIKE '%span%'")
+#prepared_train_table.write.mode("overwrite").save_as_table("drugsComTrain_prepared")
+
+
+#prepared_test_table = session.sql("SELECT * FROM drugsComTest_raw where CONDITION NOT LIKE '%span%'")
+#prepared_test_table.write.mode("overwrite").save_as_table("drugsComTest_prepared")
+
+#drugNames = session.sql("select DISTINCT(DRUGNAME) FROM drugsComTrain_prepared")
+#print(drugNames.show())
+
+#negative_rating = session.sql("SELECT DRUGNAME,COUNT(*) FROM drugsComTrain_prepared where RATING >0 and RATING<=3 and DRUGNAME =")
+#print(negative_rating.show())
+
+#neutral_rating = session.sql("SELECT COUNT(*) FROM drugsComTrain_prepared where RATING >3 and RATING<=7 and DRUGNAME ='Harvoni'").collect()
+#print(neutral_rating)
+#positive_rating = session.sql("SELECT DRUGNAME, COUNT(*) FROM drugsComTrain_prepared where RATING >7 and RATING<=10 and DRUGNAME =")
+#print(positive_rating.show())
+
+
+
+
+def get_train_data():
+    traindata = session.table("drugsComTrain_prepared")
+    return traindata
+
+def get_test_data():
+    testdata =  session.table("drugsComTest_prepared")
+    return testdata
--- src/webApp.py
+++ src/webApp.py
@@ -3,26 +3,49 @@ from snowpark import *
 
 def main():
     st.title("SNOWFLAKE HACKATHON - DRUG REVIEW")
-    tab1, tab2 = st.tabs(["Exploratory Data Analysis", "Prediction"])
+    tab1, tab2,tab3 = st.tabs(["Drugs & Conditions","Ratings","Recommendation"])
 
     with tab1:
         session = create_session()
-        CommonConditonsinPatients = session.sql("SELECT CONDITION, count(*) FROM drugsComTrain_raw GROUP BY CONDITION ORDER BY count(*) DESC").to_df("Conditions", "Count")
+        CommonConditonsinPatients = session.sql("SELECT CONDITION, count(*) FROM drugsComTrain_prepared GROUP BY CONDITION ORDER BY count(*) DESC limit 100").to_df("Conditions", "Count")
+        CommonConditonsinPatients = CommonConditonsinPatients.na.drop()
         st.write("Most Common Conditions in Patients")
         st.bar_chart(data=CommonConditonsinPatients, x=CommonConditonsinPatients.columns[0],y=CommonConditonsinPatients.columns[1])
         st.write("Drug Availability for patient conditions")
-        drugAvailabilityForConditions = session.sql("SELECT COUNT(DISTINCT DRUGNAME) as drugavailable, CONDITION FROM drugsComTrain_raw GROUP BY CONDITION HAVING COUNT(DISTINCT DRUGNAME) >1").to_df("DrugAvailable", "Conditions")
+        drugAvailabilityForConditions = session.sql("SELECT COUNT(DISTINCT DRUGNAME) as drugavailable, CONDITION FROM drugsComTrain_prepared GROUP BY CONDITION HAVING COUNT(DISTINCT DRUGNAME) >1 limit 100").to_df("DrugAvailable", "Conditions")
+        drugAvailabilityForConditions = drugAvailabilityForConditions.na.drop()
         st.bar_chart(data=drugAvailabilityForConditions,x=drugAvailabilityForConditions.columns[1],y=drugAvailabilityForConditions.columns[0])
         st.write("Drugs which can be used for many conditions")
-        drugUsage = session.sql("SELECT COUNT(DISTINCT CONDITION) as drugusagecount, DRUGNAME FROM drugsComTrain_raw GROUP BY DRUGNAME HAVING COUNT(DISTINCT CONDITION) >1").to_df("drugusagecount","Drugname")
+        drugUsage = session.sql("SELECT COUNT(DISTINCT CONDITION) as drugusagecount, DRUGNAME FROM drugsComTrain_prepared GROUP BY DRUGNAME HAVING COUNT(DISTINCT CONDITION) >1 limit 100").to_df("drugusagecount","Drugname")
+        drugUsage = drugUsage.na.drop()
         st.bar_chart(data=drugUsage, x=drugUsage.columns[1],y=drugUsage.columns[0])
 
     with tab2:
-        #pass this patientCondition to a model and get a response
-        patientCondition = st.text_input('Patient Condition: ', '')
-        st.write("The input for model is ", patientCondition)
-        st.write("The drug recommended for ", patientCondition, "are")
+        drugNames = session.sql("select DISTINCT(DRUGNAME) FROM drugsComTrain_prepared limit 100")
+        option1 = st.selectbox(
+            'Drug you like to know about ?',
+            (drugNames))
 
+        st.write('You selected:', option1)
+        negative_rating = session.sql(f"SELECT COUNT(*) as No_of_Negative_ratings FROM drugsComTrain_prepared where RATING >0 and RATING<=3 and DRUGNAME = '{option1}'")
+
+        neutral_rating = session.sql(f"SELECT  COUNT(*) as No_of_Neutral_ratings FROM drugsComTrain_prepared where RATING >3 and RATING<=7 and DRUGNAME ='{option1}'")
+
+        positive_rating = session.sql(f"SELECT COUNT(*) as No_of_Positive_ratings FROM drugsComTrain_prepared where RATING >7 and RATING<=10 and DRUGNAME ='{option1}'")
+
+        st.table(data=positive_rating)
+        st.table(data=neutral_rating)
+        st.table(data=negative_rating)
+
+
+    with tab3:
+        Conditions = session.sql("select DISTINCT(CONDITION) FROM drugsComTrain_prepared limit 100")
+        option2 = st.selectbox(
+            'Please choose the patient condition',
+            (Conditions))
+        topdrugs = session.sql(f"select drugname from drugsComTrain_prepared WHERE CONDITION = '{option2}' group by drugname order by avg(RATING) desc limit 5")
+        st.write("The top 5 recommended drugs for ",option2," are ")
+        st.table(data=topdrugs)
 
 if __name__ == '__main__':
     main()
